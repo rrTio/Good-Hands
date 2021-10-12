2f0dc2e12b3d9027b36b1f0454557afe4f80fc97
rrTio <tio.rozsraven@ue.edu.ph>
2021-10-12 22:26:28 +0800

    Sidebar test update

--- pages/Sidebar.js
+++ pages/Sidebar.js
@@ -36,7 +36,7 @@ const Sidebar = () => {
           </button>
         </div>
 
-        <div className="sidebar border-r-2 border-black text-blue-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
+        <div className="sidebar max-h-full border-r-2 border-black text-blue-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
             <h1>
               <span className="text-2xl font-extrabold text-black">
                 GoodHands
