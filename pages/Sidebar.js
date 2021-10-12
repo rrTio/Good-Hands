8d7263b7ff287d1539b23d847bbf3daa486b1337
rrTio <tio.rozsraven@ue.edu.ph>
2021-10-12 22:19:47 +0800

    Sidebar test update

--- pages/Sidebar.js
+++ pages/Sidebar.js
@@ -9,9 +9,11 @@ const Sidebar = () => {
   
   return (
     <div>
-      <div className={"relative min-h-screen md:flex"}>
+      <div className={"relative min-h-screen md:flex "}>
         <div
-          className={"bg-gray-800 text-gray-100 flex justify-between md:hidden"}
+          className={
+            "bg-gray-800 text-gray-100 flex justify-between md:hidden "
+          }
         >
           <center>
             <h1>GoodHands</h1>
@@ -35,24 +37,24 @@ const Sidebar = () => {
         </div>
 
         <div className="sidebar border-r-2 border-black text-blue-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
-          <h1>
-            <span className="text-2xl font-extrabold text-black">
-              GoodHands
-            </span>
-          </h1>
-          <nav>
-            <Link href="./">
-              <a className="block py-2.5 px-4 rounded transition duration-200 text-black hover:bg-green-300 bg-opacity-100 hover:text-black">
-                Dashboard
-              </a>
-            </Link>
+            <h1>
+              <span className="text-2xl font-extrabold text-black">
+                GoodHands
+              </span>
+            </h1>
+            <nav>
+              <Link href="./">
+                <a className="block py-2.5 px-4 rounded transition duration-200 text-black hover:bg-green-300 bg-opacity-100 hover:text-black">
+                  Dashboard
+                </a>
+              </Link>
 
-            <Link href="./Booking">
-              <a className="block py-2.5 px-4 rounded transition duration-200 text-black hover:bg-green-300 bg-opacity-100 hover:text-black">
-                Booking
-              </a>
-            </Link>
-          </nav>
+              <Link href="./Booking">
+                <a className="block py-2.5 px-4 rounded transition duration-200 text-black hover:bg-green-300 bg-opacity-100 hover:text-black">
+                  Booking
+                </a>
+              </Link>
+            </nav>
         </div>
       </div>
     </div>
